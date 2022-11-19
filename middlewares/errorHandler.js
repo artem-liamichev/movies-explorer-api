0f71e901a87431a94721e325f554bc330c6a8571
artem-liamichev <76532860+artem-liamichev@users.noreply.github.com>
2022-11-19 10:23:01 +0300

    celebrate check is excluded from errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,6 @@
-const { isCelebrateError } = require('celebrate');
-
 const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message = 'Неизвестная ошибка сервера' } = err;
-  if (isCelebrateError(err)) {
-    res.status(statusCode).json(err);
-  } else {
-    res.status(statusCode).json({ message: statusCode === 500 ? 'Неизвестная ошибка сервера' : message });
-  }
+  res.status(statusCode).json({ message: statusCode === 500 ? 'Неизвестная ошибка сервера' : message });
   next();
 };
 
